6882db4fb8a05870615ac8cafd6dfc2a15b62ab5
Jalh <championthibautfr@gmail.com>
2019-02-19 16:08:02 +0100

    Exit Safety

--- app/src/main/java/com/desrumaux/androidtoolbox/model/Server/ServerAPI.kt
+++ app/src/main/java/com/desrumaux/androidtoolbox/model/Server/ServerAPI.kt
@@ -1,7 +1,6 @@
 package com.desrumaux.androidtoolbox.model.Server
 
 import android.app.Activity
-import android.widget.Toast
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
@@ -12,8 +11,6 @@ import kotlin.system.exitProcess
 open class ServerAPI(context: Activity){
 
     private val activity: Activity = context
-    private  var token: String ?= null
-    private var key : String ?= null
 
     init {
         getInfoFromServer()
